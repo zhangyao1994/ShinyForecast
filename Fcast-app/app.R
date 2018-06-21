1e50f182cd601456889e8e00f6dca0864586a150
Yao Zhang <zhangyao1994@gmail.com>
2018-06-21 16:36:03 -0500

    Output Plotly but no rect.

--- Fcast-app/app.R
+++ Fcast-app/app.R
@@ -11,11 +11,11 @@ ui <- fluidPage(
                              choices = Region.groups, selected = "APJ"),
                  selectInput("Model", h3("Model Selection"), 
                              choices = Model.groups[c(1,4:8)], selected = "Prophet"),
-                 submitButton("Submit")
+                 submitButton("Submit"), width = 3
     ),
     mainPanel(#img(src = "ESG_HDD_SAS12G_1_2TB_10K_2_5 HDD Weekly Sales.png",width = 1200),
               #textOutput("selected_var"),
-              plotOutput("selected_plot")
+              plotlyOutput("selected_plot"),width = 9
     )
   )
 )
@@ -23,16 +23,21 @@ ui <- fluidPage(
 # Define server logic ----
 server <- function(input, output) {
   
-  output$selected_plot <- renderPlot({
+  output$selected_plot <- renderPlotly({
       # for certain CFG
-      ggplot(filter(CFG_fcast.joined,CFG==input$CFG), aes(x=Fiscal_Wk,y=HDD_QTY,group = Model, color = Model)) +
+      ggplotly(ggplot(filter(CFG_fcast.joined,CFG==input$CFG), aes(x=Fiscal_Wk,y=HDD_QTY,group = Model, color = Model)) +
       geom_point(size = 2) +
       geom_line(size = 1.5,alpha=0.6) +
       labs(title = paste(input$CFG,'Weekly Sales'), x = "Fiscal Week", y = "Part Quantity") +
       theme_minimal(base_size = 14) +
       scale_color_tableau('tableau10medium') +
       scale_x_discrete(breaks = c('FY17W01', 'FY18W01', 'FY19W01')) +
-      scale_y_continuous(label=comma) + expand_limits(y = 0)
+      scale_y_continuous(label=comma) + expand_limits(y = 0))# +
+      #annotate("text", x = 'FY19W01', y = 25, label = "Forecast Region") #+
+      # geom_rect(xmin = 52*2+7,#'FY19W07',
+      #           xmax = 52*2+18,#'FY19W18',
+      #           ymin = 0, ymax = 45000,
+      #           fill = palette_light()[[4]], alpha = 0.01)
   })
 }
 
