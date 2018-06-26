2ed816cfd0f9655c03996c69eee7cbd7b0d8e8ae
Yao Zhang <zhangyao1994@gmail.com>
2018-06-26 12:00:43 -0500

    Region Selection. Done.

--- Fcast-app/app.R
+++ Fcast-app/app.R
@@ -1,9 +1,18 @@
 library(shiny)
+library(tidyverse)
+library(ggthemes)
+options(scipen = 999)
+library(lubridate)
+library(tidyquant)
+library(plotly)
+library(scales) # for percent
+
 #setwd("~/GitHub/ShinyPractice/Fcast-app/data")
 CFG_fcast.joined <- readRDS('CFG_fcast_joined.rds')
 CFGgroups <- levels(factor(CFG_fcast.joined$CFG))
-Region.groups <- levels(factor(CFG_fcast.joined$RGN_DESC))
-Model.groups <- levels(factor(CFG_fcast.joined$Model))
+Regions <- levels(factor(CFG_fcast.joined$Region))
+Models <- levels(factor(CFG_fcast.joined$Model))
+# CFG_fcast.joined$Region <- as.factor(CFG_fcast.joined$Region)
 
 # Define UI ----
 ui <- fluidPage(
@@ -13,14 +22,12 @@ ui <- fluidPage(
     sidebarPanel(selectInput("CFG", h3("CFG Selection"), 
                              choices = CFGgroups, selected = "ESG_HDD_SAS12G_1_2TB_10K_2_5"),
                  selectInput("Region", h3("Region Selection"), 
-                             choices = Region.groups, selected = "APJ"),
+                             choices = Regions),
+                 checkboxInput('AllRegions','All Regions'),
                  selectInput("Model", h3("Model Selection"), 
-                             choices = Model.groups[c(1,4:8)], selected = "Prophet"),
-                 submitButton("Submit"), width = 3
+                             choices = Models[c(1,4:8)], selected = "Prophet")
     ),
-    mainPanel(#img(src = "ESG_HDD_SAS12G_1_2TB_10K_2_5 HDD Weekly Sales.png",width = 1200),
-              #textOutput("selected_var"),
-              plotlyOutput("selected_plot"),width = 9
+    mainPanel(plotlyOutput("selected_plot"), width = 7
     )
   )
 )
@@ -29,20 +36,30 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   output$selected_plot <- renderPlotly({
-      # for certain CFG
-      ggplotly(ggplot(filter(CFG_fcast.joined,CFG==input$CFG), aes(x=Fiscal_Wk,y=HDD_QTY,group = Model, color = Model)) +
-      geom_point(size = 2) +
-      geom_line(size = 1.5,alpha=0.6) +
-      labs(title = paste(input$CFG,'Weekly Sales'), x = "Fiscal Week", y = "Part Quantity") +
-      theme_minimal(base_size = 14) +
-      scale_color_tableau('tableau10medium') +
-      scale_x_discrete(breaks = c('FY17W01', 'FY18W01', 'FY19W01')) +
-      scale_y_continuous(label=comma) + expand_limits(y = 0))# +
-      #annotate("text", x = 'FY19W01', y = 25, label = "Forecast Region") #+
-      # geom_rect(xmin = 52*2+7,#'FY19W07',
-      #           xmax = 52*2+18,#'FY19W18',
-      #           ymin = 0, ymax = 45000,
-      #           fill = palette_light()[[4]], alpha = 0.01)
+    # for certain CFG and Region
+    p <- ggplotly(ggplot(filter(CFG_fcast.joined,CFG==input$CFG,Region==input$Region), aes(x=Fiscal_Wk,y=HDD_QTY,group = Model, color = Model)) +
+                    geom_point(size = 2) +
+                    geom_line(size = 1.5,alpha=0.6) +
+                    labs(title = paste(input$CFG,input$Region,'Weekly Sales'), x = "Fiscal Week", y = "Part Quantity") +
+                    theme_minimal(base_size = 14) +
+                    scale_color_tableau('tableau10medium') +
+                    scale_x_discrete(breaks = c('FY17W01', 'FY18W01', 'FY19W01')) +
+                    scale_y_continuous(label=comma) + expand_limits(y = 0))
+    if (input$AllRegions){
+      p <- output$selected_plot <- renderPlotly({
+        # for certain CFG
+        ggplotly(ggplot(filter(CFG_fcast.joined,CFG==input$CFG), aes(x=Fiscal_Wk,y=HDD_QTY,group = Model, color = Model)) +
+                   geom_point(size = 2) +
+                   geom_line(size = 1.5,alpha=0.6) +
+                   labs(title = paste(input$CFG,input$Region,'Weekly Sales'), x = "Fiscal Week", y = "Part Quantity") +
+                   theme_minimal(base_size = 14) +
+                   scale_color_tableau('tableau10medium') +
+                   scale_x_discrete(breaks = c('FY17W01', 'FY18W01', 'FY19W01')) +
+                   scale_y_continuous(label=comma) + expand_limits(y = 0) + 
+                   facet_wrap(~Region,nrow = 3, ncol = 1))
+      })
+    }
+    print(p)
   })
 }
 
