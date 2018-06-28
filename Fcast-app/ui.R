757661548311274ec88df971c797f8e049cc033a
Yao Zhang <zhangyao1994@gmail.com>
2018-06-28 15:18:04 -0500

    Mridul helped with Format!

--- Fcast-app/ui.R
+++ Fcast-app/ui.R
@@ -12,17 +12,20 @@ ui <- navbarPage(
     fluidPage(
       # Select CFG, Region, and Overall Metrics on One Row
       fluidRow(
+        br(),
+        br(),
+        br(),
         column(12,
                box(width = NULL, status = "warning",
-                 column(3,align = 'center',
+                 column(4,align = 'center',
                         selectInput("CFG", h3("CFG Selection"), 
                                     choices = CFGgroups, selected = "ESG_HDD_SAS12G_1_2TB_10K_2_5")),
-                 column(3,align = 'center',
+                 column(4,align = 'center',
                         selectInput("Region", h3("Region Selection"), 
                                     choices = Regions)),
-                 column(3,align = 'center'
-                        # selectInput("Metrics", h3("Overall Evaluation Metrics"), 
-                        #             choices = c("Attainment Rates","MAPE"))
+                 column(4,align = 'center',
+                        selectInput("Metrics", h3("Overall Metrics"), 
+                        choices = c("Attainment Rates","MAPE"))
                  )
                )
         )
@@ -83,18 +86,21 @@ ui <- navbarPage(
   tabPanel(
     'Cross-Validation', id = 'Cross-Validation',
     
-    # Boxes
+    # Select CFG, Region, and Overall Metrics on One Row
     fluidRow(
-      box(status = "primary",title = "Overview",solidHeader = T,
-          sliderInput("orders", "Orders", min = 1, max = 2000, value = 650),
-          selectInput("progress", "Progress",
-                      choices = c("0%" = 0, "20%" = 20, "40%" = 40, "60%" = 60, "80%" = 80,
-                                  "100%" = 100)
-          )
-      ),
-      box(title = "Histogram box title",
-          status = "warning", solidHeader = TRUE, collapsible = TRUE,
-          plotOutput("plot", height = 250)
+      column(12,
+             box(width = NULL, status = "warning",
+                 column(3,align = 'center',
+                        selectInput("CFG", h3("CFG Selection"), 
+                                    choices = CFGgroups, selected = "ESG_HDD_SAS12G_1_2TB_10K_2_5")),
+                 column(3,align = 'center',
+                        selectInput("Region", h3("Region Selection"), 
+                                    choices = Regions)),
+                 column(6,align = 'center',
+                        selectInput("Metrics", h3("Overall Evaluation Metrics"), 
+                                    choices = c("Attainment Rates","MAPE"))
+                 )
+             )
       )
     ),
     
---
+++ Fcast-app/ui_Mridul.R
@@ -0,0 +1,138 @@
+enableBookmarking(store = "url")
+# Define UI ----
+ui <- function(request){
+  tagList(
+    includeCSS(path = "www/AdminLTE.css"),
+    
+    #includeCSS(path = "www/shinydashboard.css"),
+    tags$style(type="text/css",
+               '.well {background-color: white;}',
+               "body {padding-top: 80px;}",
+               "body {padding-top: 50px;}",
+               ".shiny-output-error { visibility: hidden; }",
+               ".shiny-output-error:before { visibility: hidden; }",
+               '.box.box-solid.box-primary {box-shadow: 0 10px 1px rgba(0, 0, 0, 0.1);}'
+    ),
+    tags$head(
+      tags$script(
+        HTML('$(document).ready(function() {$(".treeview-menu").css("display", "block");})')
+      )
+    ),
+    
+    hidden(
+      div(
+        id = "app-content", # this is the main app content
+        
+        navbarPage( 
+          title = "HDD Forecast",
+          id = 'FcastHDD', position = 'fixed-top', fluid = T,
+          theme = shinytheme('flatly'), 
+          windowTitle = "Forecast HDD",
+          
+          tabPanel(
+            'Overview', id = 'overview',
+            
+            tags$div(id = "mydiv",
+                     br(),
+                     br()
+            ),
+            # Select CFG, Region, and Overall Metrics on One Row
+            fluidRow(
+              column(width = 12,
+                     box(width = NULL, status = "info", solidHeader = T,
+                         column(3,
+                                selectInput("CFG", "CFG Selection", 
+                                            choices = CFGgroups, selected = "ESG_HDD_SAS12G_1_2TB_10K_2_5")),
+                         column(3,
+                                selectInput("Region", "Region Selection", 
+                                            choices = Regions))
+                     )
+              )
+            ),
+            
+            fluidRow(
+              column(12,
+                     box(title = "FY19W07-FY19W18 Forecast Result Comparison",
+                         width = NULL, status = "primary", solidHeader = T,
+                         column(width = 8, 
+                                plotlyOutput("selected_plot")
+                         ),
+                         column(width = 4,
+                                h4("APE of Forecast Region (%)"),
+                                # Output: Table summarizing the values entered ----
+                                tableOutput("APEvalues"),
+                                br(),
+                                h4("Weekly Mean APE (%)"),
+                                # Output: Table summarizing the values entered ----
+                                tableOutput("APEValues_week")
+                         )
+                     )
+              )
+            ),
+            # Output the HDD Trends and APE value tables
+            
+            fluidRow(column(12,
+                            box(title = "Overall Evaluation for All CFGs",
+                                width = NULL, status = "primary", solidHeader = T,
+                                tabsetPanel(
+                                  tabPanel(str_replace_all(ResultsNames[1],'_',' '),
+                                           plotlyOutput('ErrorPlot1')
+                                  ),
+                                  tabPanel(str_replace_all(ResultsNames[2],'_',' '),
+                                           plotlyOutput('ErrorPlot2')
+                                  ),
+                                  tabPanel(str_replace_all(ResultsNames[3],'_',' '),
+                                           plotlyOutput('ErrorPlot3')
+                                  ),
+                                  tabPanel(str_replace_all(ResultsNames[4],'_',' '),
+                                           plotlyOutput('ErrorPlot4')
+                                  ),
+                                  tabPanel(str_replace_all(ResultsNames[5],'_',' '),
+                                           plotlyOutput('ErrorPlot5')
+                                  ),
+                                  tabPanel(str_replace_all(ResultsNames[6],'_',' '),
+                                           plotlyOutput('ErrorPlot6')
+                                  ),
+                                  tabPanel(str_replace_all(ResultsNames[7],'_',' '),
+                                           plotlyOutput('ErrorPlot7')
+                                  ),
+                                  tabPanel(str_replace_all(ResultsNames[8],'_',' '),
+                                           plotlyOutput('ErrorPlot8')
+                                  ),
+                                  tabPanel(str_replace_all(ResultsNames[9],'_',' '),
+                                           plotlyOutput('ErrorPlot9')
+                                  )
+                                )
+                            )
+            )
+            )
+          ),
+          
+          tabPanel(
+            'Cross-Validation', id = 'Cross-Validation',
+            
+            fluidRow(
+              column(12,
+                     br(),
+                     br(),
+                     box(title = "Four Quarterly Forecast Result Comparison", width = NULL, solidHeader = T,
+                         status = "primary",
+                         column(width = 3, align = 'center',
+                                plotlyOutput("CV_plotQ1")),
+                         column(width = 3, align = 'center',
+                                plotlyOutput("CV_plotQ2")),
+                         column(width = 3, align = 'center',
+                                plotlyOutput("CV_plotQ3")),
+                         column(width = 3, align = 'center',
+                                plotlyOutput("CV_plotQ4"))
+                     )
+              )
+            )
+          )
+        )
+      )
+    )
+  ) # end of tagList
+}
+
+
